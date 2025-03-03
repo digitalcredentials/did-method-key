f484acb44bff75dc13dcdfdad0c54289401855cb
James Chartrand <jc.chartrand@gmail.com>
2025-03-03 17:42:40 -0500

    update to match latest DB

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,9 @@
 module.exports = {
   root: true,
   extends: [
-    'eslint-config-digitalbazaar',
-    'eslint-config-digitalbazaar/jsdoc'
+    'digitalbazaar',
+    'digitalbazaar/jsdoc',
+    'digitalbazaar/module'
   ],
   env: {
     node: true,
