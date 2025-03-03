f950fb2bef1d31b5f77b5435ca3341b0b688b438
James Chartrand <jc.chartrand@gmail.com>
2025-03-03 18:06:02 -0500

    fix tests

---
+++ pre-test.js
@@ -0,0 +1,11 @@
+import PackageJson from '@npmcli/package-json';
+
+// pre-test script
+const main = async () => {
+  const pkgJson = await PackageJson.load('./');
+  pkgJson.update({ type: 'module' });
+  await pkgJson.save();
+};
+
+// run pre-test script
+main();
