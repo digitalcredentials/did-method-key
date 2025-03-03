7df499b8aea5f6cee7e066b1f46ec0d19c02b5ac
James Chartrand <jc.chartrand@gmail.com>
2025-03-03 17:42:40 -0500

    update to match latest DB

--- build-dist.sh
+++ build-dist.sh
@@ -3,6 +3,7 @@ cat >dist/esm/index.js <<!EOF
 import cjsModule from '../index.js';
 export const DidKeyDriver = cjsModule.DidKeyDriver;
 export const driver = cjsModule.driver;
+export const createFromMultibase = cjsModule.createFromMultibase
 !EOF
 
 cat >dist/esm/package.json <<!EOF
