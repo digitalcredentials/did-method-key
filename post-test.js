2708f89902c97a7f73d6dbba098378a394d8c658
James Chartrand <jc.chartrand@gmail.com>
2025-03-03 18:27:01 -0500

    fix tests and linting redux

--- post-test.js
+++ post-test.js
@@ -3,7 +3,7 @@ import PackageJson from '@npmcli/package-json';
 // post-test script
 const main = async () => {
   const pkgJson = await PackageJson.load('./');
-  pkgJson.update({ type: undefined });
+  pkgJson.update({type: undefined});
   await pkgJson.save();
 };
 
